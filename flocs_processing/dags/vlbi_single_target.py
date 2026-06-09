137c4560257f2b04ec7f586dcfb26770959a9e6e
Frits Sweijen <tikk3r@users.noreply.github.com>
2026-06-09 16:55:16 +0100

    Sort priority descending

--- flocs_processing/dags/vlbi_single_target.py
+++ flocs_processing/dags/vlbi_single_target.py
@@ -51,7 +51,7 @@ def get_db_columns():
         cursor = db.cursor()
         columns = "target_name,priority,finished,downloaded,sas_id_calibrator1,sas_id_calibrator2,sas_id_calibrator_final,sas_id_target,status_calibrator1,status_calibrator2,status_target,status_vlbi_delay,status_vlbi_dd"
         field = cursor.execute(
-            f"select {columns} from {TABLE_NAME} where finished==0 order by priority"
+            f"select {columns} from {TABLE_NAME} where finished==0 order by priority desc"
         ).fetchall()
         print(field)
     return field
