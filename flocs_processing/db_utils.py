ffc18e1d8e248f10889746ffcf6e388fb3c603ba
Frits Sweijen <tikk3r@users.noreply.github.com>
2026-09-22 08:56:35 +0100

    Fix string columns

--- flocs_processing/db_utils.py
+++ flocs_processing/db_utils.py
@@ -64,25 +64,25 @@ class FlocsDB:
         with sqlite3.connect(self.DATABASE) as db:
             cursor = db.cursor()
             cursor.execute(
-                f"update {self.TABLE_NAME} set status={FIELD_STATUS.nothing.value} where target_name=='{name}' and sas_id_target=='{target}'"
+                f"update {self.TABLE_NAME} set status='{FIELD_STATUS.nothing.value}' where target_name=='{name}' and sas_id_target=='{target}'"
             )
 
     def set_field_downloading(self, name, target):
         with sqlite3.connect(self.DATABASE) as db:
             cursor = db.cursor()
             cursor.execute(
-                f"update {self.TABLE_NAME} set status={FIELD_STATUS.downloading.value} where target_name=='{name}' and sas_id_target=='{target}'"
+                f"update {self.TABLE_NAME} set status='{FIELD_STATUS.downloading.value}' where target_name=='{name}' and sas_id_target=='{target}'"
             )
 
     def set_field_processing(self, name, target):
         with sqlite3.connect(self.DATABASE) as db:
             cursor = db.cursor()
             cursor.execute(
-                f"update {self.TABLE_NAME} set status={FIELD_STATUS.processing.value} where target_name=='{name}' and sas_id_target=='{target}'"
+                f"update {self.TABLE_NAME} set status='{FIELD_STATUS.processing.value}' where target_name=='{name}' and sas_id_target=='{target}'"
             )
 
     def set_field_finished(self, name, target):
-        query = f"update {self.TABLE_NAME} set status={FIELD_STATUS.processing.value} where target_name=='{name}' and sas_id_target=='{target}'"
+        query = f"update {self.TABLE_NAME} set status='{FIELD_STATUS.processing.value}' where target_name=='{name}' and sas_id_target=='{target}'"
         with sqlite3.connect(self.DATABASE) as db:
             cursor = db.cursor()
             cursor.execute(query)
@@ -108,7 +108,7 @@ class FlocsDB:
                 f"update {self.TABLE_NAME} set status_{identifier}={PIPELINE_STATUS.processing.value} where target_name=='{name}' and sas_id_target=='{target}'"
             )
             cursor.execute(
-                f"update {self.TABLE_NAME} set status={FIELD_STATUS.processing.value} where target_name=='{name}' and sas_id_target=='{target}'"
+                f"update {self.TABLE_NAME} set status='{FIELD_STATUS.processing.value}' where target_name=='{name}' and sas_id_target=='{target}'"
             )
 
     def set_status_await_approval(self, name, identifier, target):
